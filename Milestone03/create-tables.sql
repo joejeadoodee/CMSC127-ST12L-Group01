cc2ced0cdac0697236cc0f90427c5a268e04dedb
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2025-04-23 23:51:06 +0800

    feat: added financial obligation and payment tables

--- Milestone03/create-tables.sql
+++ Milestone03/create-tables.sql
@@ -20,16 +20,34 @@ CREATE TABLE MEMBER_DEGREE_PROGRAM(
     FOREIGN KEY(member_id, username) REFERENCES MEMBER(member_id, username)
 );
 
--- create: FINANCIAL_OBLIGATION(Org_id) table 
-
-
+-- create: FINANCIAL_OBLIGATION table 
+CREATE TABLE FINANCIAL_OBLIGATION(
+    record_id INT NOT NULL,
+    semester VARCHAR(50) NOT NULL,
+    academic_year INT(4) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    total_due INT NOT NULL,
+    due_date DATE NOT NULL,
+    amount_paid DATE NOT NULL,
+    organization_id INT NOT NULL,
+    PRIMARY KEY(record_id),
+    FOREIGN KEY(organization_id) REFERENCES ORGANIZATION(organization_id)
+);
 
 -- create: PAYMENT table
-
-
+CREATE TABLE PAYMENT(
+    payment_id INT NOT NULL,
+    amount_paid VARCHAR(50) NOT NULL,
+    payment_date INT(4) NOT NULL,
+    record_id INT NOT NULL,
+    member_id INT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    PRIMARY KEY(payment_id),
+    FOREIGN KEY(record_id) REFERENCES FINANCIAL_OBLIGATION(record_id),
+    FOREIGN KEY(member_id, username) REFERENCES MEMBER(member_id, username)
+);
 
 -- create: ORGANIZATION table
-
 
 
 -- create: SERVES table
