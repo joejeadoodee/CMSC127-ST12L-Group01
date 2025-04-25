32209d92596379619f692157c4c4b997344014c3
joejeadoodee <jalonzo@up.edu.ph>
2025-04-25 16:53:16 +0800

    chore: batch, status NOT NULL in MEMBER

--- Milestone03/create-tables.sql
+++ Milestone03/create-tables.sql
@@ -4,8 +4,8 @@ CREATE TABLE MEMBER(
     username VARCHAR(50) NOT NULL,
     name VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    batch INT,
-    status VARCHAR(50),
+    batch INT NOT NULL,
+    status VARCHAR(50) NOT NULL,
     gender CHAR(1),
     is_admin BOOLEAN DEFAULT FALSE,
     PRIMARY KEY(member_id, username)
