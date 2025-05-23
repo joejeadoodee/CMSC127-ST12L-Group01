5d25863334482cb160f0f9ad77c8d931cf4da7cf
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2025-05-23 15:11:48 +0800

    feat: added signup screens for organization and member

--- src/screens/signup.py
+++ src/screens/signup.py
@@ -1,7 +1,9 @@
 from src.decorators import screen
 from src import navigate
+import src.mariadb_connector as db
 
 # Sign up as a Member
+
 @screen
 def sign_up_member():
     print("SIGNING UP AS MEMBER...")
@@ -12,14 +14,20 @@ def sign_up_member():
     password = input("Enter password: ")
     repassword = input("Retype password: ")
 
-    # ADD VALIDATION AND UPDATE DATABASE
-
     if password != repassword:
         print("Passwords do not match! Please try again.")
         return sign_up_member()
 
-    # Assuming there's a function for inserting data into a database
-    # db.insert_member(name, username, batch, gender, password)
+    try:
+        insert_query = """
+            INSERT INTO MEMBER (username, name, password, batch, status, gender, is_admin)
+            VALUES ('{0}', '{1}', '{2}', {3}, '{4}', '{5}', {6})
+        """.format(username, name, password, int(batch), 'Active', gender, 'FALSE')
+        
+        db.cursor.execute(insert_query)
+    except Exception as e:
+        print("An error occurred while signing up:", e)
+        return
 
     print("SIGNED UP AS MEMBER!")
     print("-" * 10)
@@ -35,16 +43,23 @@ def sign_up_org():
     password = input("Enter password: ")
     repassword = input("Retype password: ")
 
-    # ADD VALIDATION AND UPDATE DATABASE
-
     if password != repassword:
         print("Passwords do not match! Please try again.")
         return sign_up_org()
 
-    # Assuming there's a function for inserting data into a database
-    # db.insert_organization(org_name, password)
+    try:
+        insert_query = """
+            INSERT INTO ORGANIZATION (name, password, number_of_members)
+            VALUES ('{0}', '{1}', {2})
+        """.format(org_name, password, 0)
+
+        db.cursor.execute(insert_query)
+    except Exception as e:
+        print("An error occurred while signing up:", e)
+        return
 
     print("SIGNED UP AS ORGANIZATION!")
     print("-" * 10)
     print("REDIRECTING TO HOME SCREEN...")
     return navigate.to_welcome()
+
