dfd63bda5e352ccd89d1835e7d7c12ae0bdf9a75
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2025-05-24 01:03:35 +0800

    feat: center window function

---
+++ src/utils.py
@@ -0,0 +1,6 @@
+def center_window(win, width, height):
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    win.geometry(f'{width}x{height}+{x}+{y}')
