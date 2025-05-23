0a8034dc12a123a07ca72e87833e54bd4b38bf8d
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2025-05-24 03:51:32 +0800

    feat: member dues without gui

--- src/screens/member/member_dues.py
+++ src/screens/member/member_dues.py
@@ -1,6 +1,9 @@
 from src.decorators import screen
 import src.mariadb_connector as db
 from src import member, navigate
+from tabulate import tabulate
+
+current_record_id = None
 
 @screen
 def view_dues():
@@ -25,10 +28,64 @@ def view_dues():
         else:
             print("Invalid input. Please try again.")
 
+
 @screen
 def view_all_dues():
-    print("MY DUES")
-    # insert code here
+    member_id = member.member_id
+
+    db.cursor.execute(f"""
+        SELECT osmfp.mem_id as 'member_id', osmfp.full_name, osmfp.record_id, osmfp.name as 'obligation_name', osmfp.org_id as 'organization_id', osmfp.org_name, osmfp.academic_year, osmfp.semester, COALESCE(SUM(osmfp.amount_paid),0)  total_amount_paid, osmfp.total_due 
+        FROM (
+            SELECT *
+            FROM (
+                SELECT *
+                FROM (
+                    SELECT DISTINCT organization_id as `org_id`, name as 'org_name', member_id as 'mem_id', full_name FROM (
+                        SELECT *
+                        FROM (SELECT * FROM ORGANIZATION) o
+                        LEFT JOIN (SELECT organization_id as org_id, member_id as 'mem_id' FROM SERVES) s
+                        ON o.organization_id=s.org_id
+                    ) os
+                    LEFT JOIN (SELECT member_id, username, name as 'full_name', password, batch, status, gender, is_admin FROM MEMBER) m
+                    ON os.mem_id=m.member_id
+                ) osm   
+                LEFT JOIN  (
+                    SELECT * 
+                    FROM FINANCIAL_OBLIGATION
+                ) fp
+                ON osm.org_id=fp.organization_id
+            ) r
+            LEFT JOIN (
+                SELECT payment_id, amount_paid, payment_date, record_id as 'p_record_id', member_id FROM PAYMENT
+            ) p
+            ON p.member_id = r.mem_id
+        ) osmfp 
+        GROUP BY member_id, record_id, organization_id 
+        HAVING total_amount_paid < total_due AND osmfp.mem_id = {member_id};
+    """)
+    
+    rows = db.cursor.fetchall()
+
+    headers = ["Organization", "Obligation", "Academic Year", "Semester", "Amount Paid", "Total Due"]
+    table_data = []
+
+    for row in rows:
+        obligation_name = row[3]
+        organization_name = row[5]
+        academic_year = row[6]
+        semester = row[7]
+        total_amount_paid = row[8]
+        total_due = row[9]
+
+        table_data.append([
+            organization_name, obligation_name, academic_year, semester, total_amount_paid, total_due
+        ])
+
+    if table_data:
+        print(tabulate(table_data, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("You have no outstanding dues!")
+
 
 @screen
 def pay_due():
@@ -38,5 +95,60 @@ def pay_due():
 @screen
 def view_payment_history():
     print("MY PAYMENT HISTORY")
+    member_id = member.member_id
+
+    db.cursor.execute(f"""
+        SELECT osmfp.mem_id as 'member_id', osmfp.full_name, osmfp.record_id, osmfp.name as 'obligation_name', osmfp.org_id as 'organization_id', osmfp.org_name, osmfp.academic_year, osmfp.semester, COALESCE(SUM(osmfp.amount_paid),0)  total_amount_paid, osmfp.total_due 
+        FROM (
+            SELECT *
+            FROM (
+                SELECT *
+                FROM (
+                    SELECT DISTINCT organization_id as `org_id`, name as 'org_name', member_id as 'mem_id', full_name FROM (
+                        SELECT *
+                        FROM (SELECT * FROM ORGANIZATION) o
+                        LEFT JOIN (SELECT organization_id as org_id, member_id as 'mem_id' FROM SERVES) s
+                        ON o.organization_id=s.org_id
+                    ) os
+                    LEFT JOIN (SELECT member_id, username, name as 'full_name', password, batch, status, gender, is_admin FROM MEMBER) m
+                    ON os.mem_id=m.member_id
+                ) osm   
+                LEFT JOIN  (
+                    SELECT * 
+                    FROM FINANCIAL_OBLIGATION
+                ) fp
+                ON osm.org_id=fp.organization_id
+            ) r
+            LEFT JOIN (
+                SELECT payment_id, amount_paid, payment_date, record_id as 'p_record_id', member_id FROM PAYMENT
+            ) p
+            ON p.member_id = r.mem_id
+        ) osmfp 
+        GROUP BY member_id, record_id, organization_id
+        HAVING osmfp.mem_id = {member_id}
+        ORDER BY osmfp.academic_year;
+    """)
+    
+    rows = db.cursor.fetchall()
+
+    headers = ["Organization", "Obligation", "Academic Year", "Semester", "Amount Paid", "Total Due"]
+    table_data = []
+
+    for row in rows:
+        obligation_name = row[3]
+        organization_name = row[5]
+        academic_year = row[6]
+        semester = row[7]
+        total_amount_paid = row[8]
+        total_due = row[9]
+
+        table_data.append([
+            organization_name, obligation_name, academic_year, semester, total_amount_paid, total_due
+        ])
+
+    if table_data:
+        print(tabulate(table_data, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("You have no outstanding dues!")
     # insert code here
 
